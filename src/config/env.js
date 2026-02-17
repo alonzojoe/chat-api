1646f08de1da905c3839e13a363801a3ea57afac
alonzojoe <alonzojoenell2k15@gmail.com>
2026-02-17 11:40:05 +0800

    chore: update env fallback url

--- src/config/env.js
+++ src/config/env.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 
 export const env = {
   port: Number(process.env.PORT || 4000),
-  corsOrigin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+  corsOrigin: process.env.CORS_ORIGIN || 'http://localhost:3000',
 
   mongo: {
     uri: process.env.MONGO_URI || "mongodb://127.0.0.1:27017/chat_db",
